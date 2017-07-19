9f4dbf6aa45f3e81d90b7867d498ed9e8f45fbb5
Michael Collins <mscoll85@gmail.com>
2017-07-19 01:10:19 -0400

    lowercased

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,21 +6,21 @@ const config = require('../config')
 
 const msgDefaults = {
   response_type: 'in_channel',
-  username: 'PolyBot',
+  username: 'polybot',
   icon_emoji: config('ICON_EMOJI')
 }
 
 let attachments = [
   {
-    title: 'PolyBot will help you find the hippest repos on GitHub',
+    title: 'polybot will help you find the hippest repos on GitHub',
     color: '#2FA44F',
-    text: '`/PolyBot repos` returns hip repos \n`/PolyBot javascript` returns hip JavaScript repos',
+    text: '`/polybot repos` returns hip repos \n`/polybot javascript` returns hip JavaScript repos',
     mrkdwn_in: ['text']
   },
   {
-    title: 'Configuring PolyBot',
+    title: 'Configuring polybot',
     color: '#E3E4E6',
-    text: '`/PolyBot help` ... you\'re lookin at it! \n',
+    text: '`/polybot help` ... you\'re lookin at it! \n',
     mrkdwn_in: ['text']
   }
 ]
